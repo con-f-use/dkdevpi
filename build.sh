c69753a9866f3324771f7f55704fd19663018bf8
con-f-use <con-f-use@gmx.net>
2018-12-26 00:52:38 +0100

    Include repo version in webui and enable themes

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 export GIT_VERSION=$(git rev-parse --abbrev-ref HEAD).$(git describe --always --dirty --abbrev)
 export GIT_SHA=$(git rev-parse HEAD)
 export BUILD_DATE=$(date --rfc-3339=seconds)
-docker-compose build
+
+docker-compose stop nginx devpi || true
+docker-compose build &&
+  docker-compose up -d
 
--- devpi/run.sh
+++ devpi/run.sh
@@ -1,12 +1,16 @@
 
 serverdir=/devpi/server
+default_config=/devpi/dataindex.json
 
 if [ ! -e "$serverdir/.serverversion" ]; then
-    debpi-server --init --serverdir "$serverdir"
+    devpi-server --serverdir "$serverdir" --init
+    devpi-server --serverdir "$serverdir" --import "$default_config"
+    rm -f "$default_config"
 fi
 
 devpi-server \
   --host 0.0.0.0 \
   --port 4040 \
   --serverdir "$serverdir" \
-  --restrict-modify root
+  --restrict-modify root \
+  --theme /devpi/
